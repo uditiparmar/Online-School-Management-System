3f797d3b9cce790aee4dd9dd43b46d399c5586db
Uditi Parmar <uditiparmar18@gmail.com>
2024-07-11 19:12:06 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Management System</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+    
+    <nav>
+        <label class="logo">W-School</label>
+
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Admission</a></li>
+            <li><a href="">Login</a></li>
+        </ul>
+    </nav>
+
+    <div class="section1">
+
+        <label class="img_text">WE TEACH STUDENTS WITH CARE</label>
+        <img class="main_img" src="sms.jpg">
+    </div>
+
+     <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="welcome_img" src="school.jpg">
+            </div>
+            <div class="col-md-4">
+                <h1>Welcome to W-School</h1>
+            </div>
+
+        </div>
+
+    </div> 
+    <br><br>
+    <center>
+       <h1>Our Teachers</h1> 
+       <br>
+    </center>
+    <section class="items">
+        <div class="items">
+            <img class="teacher_img" src="teacher1.jpg">
+
+        </div>
+        <div class="items">
+            <img class="teacher_img" src="teacher2.jpg">
+
+        </div>
+        <div class="items">
+            <img class="teacher_img" src="teacher3.jpg">
+
+        </div>
+
+    </section>
+    <br><br><br>
+
+    <center>
+       <h1>Our Courses</h1> 
+       <br>
+    </center>
+    <section class="items">
+        <div class="items">
+            <img class="teacher_img" src="web4.jpg">
+            
+
+        </div>
+        <div class="items">
+            <img class="teacher_img" src="web5.jpg">
+            
+
+        </div>
+        <div class="items">
+            <img class="teacher_img" src="web6.jpg">
+            
+
+        </div>
+
+    </section>
+    <br><br>
+
+    <center>
+        <h1>Admission Form</h1>
+        <br>
+    </center>
+
+    <div align="center" class="admission_form">
+        <form>
+            <div>
+                <label class="label_text">Name</label>
+                <input class="input_deg" type="text" name="">
+            </div>
+
+            <div>
+                <label class="label_text">Email</label>
+                <input class="input_deg" type="text" name="">
+            </div>
+
+            <div>
+                <label class="label_text">Phone</label>
+                <input class="input_deg" type="text" name="">
+            </div>
+
+            <div>
+                <label class="label_text">Message</label>
+                <textarea></textarea>
+            </div>
+            <br>
+            <div>
+                
+                <input type="Submit">
+            </div>
+        </form>
+
+    </div>
+</body>
+</html> 
